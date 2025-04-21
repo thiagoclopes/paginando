00a7415d49df43e6479c7b19c4cd43da1e38ee72
Thiago Lopes <thiagoviniciusc33@gmail.com>
2025-04-21 20:29:15 -0300

    updating

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,7 +17,7 @@ export const Header = (props) => {
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Learn More
+                  Saiba mais
                 </a>{" "}
               </div>
             </div>
